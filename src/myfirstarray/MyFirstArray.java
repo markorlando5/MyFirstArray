1cd9298aa792237efb399983f7bc4f491b30c244
xcode <xcode@192.168.0.19>
2018-12-21 17:02:01 -0500

    Counts backwards

--- src/myfirstarray/MyFirstArray.java
+++ src/myfirstarray/MyFirstArray.java
@@ -17,14 +17,18 @@ public class MyFirstArray {
     public static void main(String[] args) {
         int n, sum = 0;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter word: ");
+        System.out.print("Enter number: ");
         n = s.nextInt();
         int a[] = new int[n];
         System.out.println("Enter all the elements: ");
-        for(int i =0; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
             a[i] = s.nextInt();
             sum = sum + a[i];
+        }
+        for(int i = n-1; i >= 0; i--)
+        {
+            System.out.println(a[i]);
         }
         System.out.println("Sum: " + sum);
     }
